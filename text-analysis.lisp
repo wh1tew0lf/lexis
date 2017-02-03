e91ecf52113442997bc1bb7c60bf2b6a125778ba
Volkov Danil <volkovdanil91@gmail.com>
2017-02-03 17:32:05 +0600

    Untabify applied

--- text-analysis.lisp
+++ text-analysis.lisp
@@ -2,29 +2,29 @@
   "Returns hash-table with downcased letters as keys and frequencies as values. Container optional"
   (when (null container) (setf container (make-hash-table :test 'equal)))
   (loop for chr across text do
-	   (let* ((lchar (char-downcase chr))
-			  (frequency (gethash lchar container 0)))
-		 (setf (gethash lchar container) (+ 1 frequency)))
-	 finally (return-from get-letter-frequency container)))
+       (let* ((lchar (char-downcase chr))
+              (frequency (gethash lchar container 0)))
+         (setf (gethash lchar container) (+ 1 frequency)))
+     finally (return-from get-letter-frequency container)))
 
 
 (defun get-n-gramm(text &optional (n 2) (container nil))
   "Returns hash-table with downcased n-grams as keys and frequencies as values"
   (when (null container) (setf container (make-hash-table :test 'equal)))
   (loop
-	 for chr across text
-	 for ngram = ""
-	 then (let
-			  ((lngram ngram) (lchar (char-downcase chr)))
-			(when (alpha-char-p lchar)
-			  (if (= (length lngram) n)
-				  (setf lngram (concatenate 'string (subseq lngram 1) (list lchar)))
-				  (setf lngram (concatenate 'string lngram (list lchar))))
-			  (if (= (length lngram) n)
-				  (let ((frequency (gethash lngram container 0)))
-					(setf (gethash lngram container) (+ 1 frequency)))))
-			lngram)
-	 finally (return-from get-n-gramm container)))
+     for chr across text
+     for ngram = ""
+     then (let
+              ((lngram ngram) (lchar (char-downcase chr)))
+            (when (alpha-char-p lchar)
+              (if (= (length lngram) n)
+                  (setf lngram (concatenate 'string (subseq lngram 1) (list lchar)))
+                  (setf lngram (concatenate 'string lngram (list lchar))))
+              (if (= (length lngram) n)
+                  (let ((frequency (gethash lngram container 0)))
+                    (setf (gethash lngram container) (+ 1 frequency)))))
+            lngram)
+     finally (return-from get-n-gramm container)))
 
 (defun hash-table-alist (table)
   "Returns an association list containing the keys and values of hash table TABLE."
@@ -36,16 +36,16 @@
 
 (defun print-hash-table (table)
   (loop for k being the hash-keys in table using (hash-value v)
-	 do (format t "~A ~A ~%" k v)))
+     do (format t "~A ~A ~%" k v)))
 
 (defun print-sort-by-key (table)
   (let ((alist (hash-table-alist table))
-		(cnt (loop
-				for k being the hash-keys in table
-				using (hash-value v)
-				sum v)))
+        (cnt (loop
+                for k being the hash-keys in table
+                using (hash-value v)
+                sum v)))
 
-	(loop for (chr . frq) in (sort alist #'string< :key (lambda(p) (string (car p))))
+    (loop for (chr . frq) in (sort alist #'string< :key (lambda(p) (string (car p))))
       do (format t "~A ~,2F%~%" chr (/ (* 100.0 frq) cnt)))))
 
 (defun print-sort-by-frequency (table)
